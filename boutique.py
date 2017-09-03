757d68a32deef87de81ef6da52fabb91fc13be09
acheamponge <acheamponge@berea.edu>
2017-09-03 02:46:44 +0000

    csc 420

--- boutique.py
+++ boutique.py
@@ -1,21 +1,18 @@
 from googlefinance import getQuotes
 import json
 import csv
-import sys
 import pandas
+
 stocks = ['AAPL','GOOGL', 'AMZN', 'FB', 'SSNLF', 'MSFT', 'SNE', 'IBM', 'NTDOY', 'ORCL', 'VMW', 'INTC', 'HPQ', 'DVMT',
-'CSCO', 'ADBE', 'SAP', 'LNVGY', 'RHT', 'SYMC', 'TWTR']
+'CSCO', 'ADBE', 'SAP', 'LNVGY', 'RHT', 'SYMC', 'TWTR', 'ATNY', 'ACTS', 'ATVI', 'IOTS', 'ADRO', 'AMD', 'ADVS', 'ZBRA', 
+'XPLR', 'XBIT', 'WIX', 'WBMD', 'VNOM', 'VIA', 'VECO', 'UPLD', 'UBNT', 'UFPT', 'TYME', 'TRIV', 'TRIP', 'TGA', 'TSI',
+'TESS', 'AIR', 'ATEN', 'AQN', 'DDD', 'WUBA', 'ASX', 'A', 'AYX', 'AMBR', 'AEE', 'BHE', 'BIO', 'CPL', 'CTS', 'CMI', 'DGI',
+'D', 'HEI', 'ASUR', 'AVID', 'EPAY', 'CBAK', 'CPSH', 'CYAN', 'DBVT', 'DELT', 'EDGW', 'XELA', 'FALC', 'SVVC', 'FBIO', 'GEOS',
+'QQQC', 'GPRO', 'MRVL', 'MZOR', 'BKFS', 'BA', 'CAJ', 'CRY', 'CUB', 'DGI', 'GME', 'GM', 'GNE', 'ASR', 'PAC', 'MDT',
+'MTD', 'MIXT', 'MULE', 'NC', 'NPTN', 'NXPI', 'NSTG', 'UEPS']
 
 stock_json = json.dumps(getQuotes(stocks), indent=2)    
 
-#stock_load = json.loads(stock_json)
+stock_df = pandas.read_json(stock_json)
 
-#output = csv.writer(sys.stdout)
-
-#output.writerow(stock_load[0].keys())
-
-#for row in stock_load:
-    #output.writerow(row.values())
-df = pandas.read_json(stock_json)
-
-df.to_csv("stock.csv")
+stock_df.to_csv("stock.csv")

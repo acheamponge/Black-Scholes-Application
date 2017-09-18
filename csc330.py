cc14923a5f28f156b2abef3f4829d6869f915e05
Emmanuel Acheampong <acheamponge@berea.edu>
2017-09-18 11:18:16 -0500

    Update csc330.py

--- csc330.py
+++ csc330.py
@@ -1,7 +1,11 @@
+  GNU nano 2.2.6                                File: csc330.py                                                                       
+
 import pandas_datareader as pdr
 import csv
 import pandas
 import json
+from pandas_datareader.data import Options
+from datetime import date
 
 stocks = ['AAPL','GOOGL', 'AMZN', 'FB', 'SSNLF', 'MSFT', 'SNE', 'IBM', 'NTDOY', 'ORCL', 'VMW', 'INTC', 'HPQ', 'DVMT',
 'CSCO', 'ADBE', 'SAP', 'LNVGY', 'RHT', 'SYMC', 'TWTR', 'ATNY', 'ACTS', 'ATVI', 'IOTS', 'ADRO', 'AMD', 'ADVS', 'ZBRA',
@@ -13,6 +17,9 @@ stocks = ['AAPL','GOOGL', 'AMZN', 'FB', 'SSNLF', 'MSFT', 'SNE', 'IBM', 'NTDOY', 
 
 stock_df = pdr.get_quote_yahoo(stocks)
 
+
+#s = Options('AAPL')._get_data_in_date_range(date.today())
+#print s
 stock_df.index.name = "StockSymbol"
 
 stock_df.to_csv("stock.csv")
@@ -21,5 +28,7 @@ company_stock = pandas.read_csv("stock.csv")
 companies = pandas.read_csv("companies.csv")
 
 merged = company_stock.merge(companies, on = 'StockSymbol')
-
-merged.to_csv('new.csv', index=False)
+merged['Amount'] = merged['last']*(merged['NumberofStocks']).astype(float)
+merged.columns = map(str.lower, merged.columns)
+merged.rename(columns = {'change_pct':'changepct','short_ratio' : 'shortratio'}, inplace = True)
+merged.to_csv('new_stock.csv', index=False)
